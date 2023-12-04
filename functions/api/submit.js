3bdc44a6bc808e7f2abadcb219217cd9f7369e6b
Quentin Henry <quentin.henry@lg2.com>
2023-12-04 09:25:39 -0500

    add dato

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,14 +2,20 @@
  * POST /api/submit
  */
 
-import axios from 'axios';
+import { buildClient } from '@datocms/cma-client-node';
 
 export async function onRequestPost(context) {
 
     try {
-        const res = await axios("https://swapi.dev/api/");
+        const client = buildClient({ apiToken: process.env.DATO_API_TOKEN });
+        const modelIdOrApiKey = 'ssf_page_home';
+        const fields = await client.fields.list(modelIdOrApiKey);
 
-        return new Response(res.data, {
+        fields.forEach((field) => {
+            console.log(field);
+        });
+
+        return new Response(field, {
             headers: {
                 'Content-Type': 'application/json;charset=utf-8',
             },
