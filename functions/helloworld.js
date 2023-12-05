19a24c85429628cd66d777f82691ed8a67b33292
Quentin Henry <quentin.henry@lg2.com>
2023-12-05 15:24:28 -0500

    retry dato client package

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,11 @@
-export function onRequest(context) {
+import { buildClient } from '@datocms/cma-client-node';
+
+export async function onRequest(context) {
+
+    const client = buildClient({ apiToken: process.env.DATO_API_TOKEN });
+    const site = await client.site.findFirst();
+
+    console.log(site);
+
     return new Response("Hello, world!")
 }
